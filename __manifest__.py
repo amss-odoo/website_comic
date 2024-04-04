7197cce7c9a3d53e56a4d56755ceba5b89541b4b
amss-odoo <amss@odoo.com>
2024-04-04 12:47:33 +0530

    added login page

--- __manifest__.py
+++ __manifest__.py
@@ -16,8 +16,10 @@
         'views/footer.xml',
         'views/snippets/options.xml',
         'views/snippets/s_anime_hero.xml',
+        'views/snippets/s_image_text.xml',
 
         'data/pages/categories.xml',
+        'data/pages/login.xml'
     ],
     'assets': {
         'web._assets_primary_variables': [
@@ -27,9 +29,11 @@
             'website_comic/static/src/scss/bootstrap_overridden.scss',
         ],
         'web.assets_frontend': [
-            # 'website_comic/static/src/scss/header.scss',
+            'website_comic/static/src/scss/header.scss',
             'website_comic/static/src/scss/footer.scss',
-            'website_comic/static/src/scss/categories.scss'
+            'website_comic/static/src/scss/categories.scss',
+            'website_comic/static/src/snippets/s_anime_hero.scss',
+            'website_comic/static/src/scss/login.scss',
         ]
     },
 }
